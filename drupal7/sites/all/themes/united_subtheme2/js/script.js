81f7e1945cc83dc1353b19a930bbcd56b434c4ae
elperg1 <elliot.pergl@gmail.com>
2013-08-28 18:01:02 -0400

    JS merged
    left nav working

--- drupal7/sites/all/themes/united_subtheme2/js/script.js
+++ drupal7/sites/all/themes/united_subtheme2/js/script.js
@@ -337,6 +337,56 @@
 
 	});
 
+// Email Code
+
+	function rawurlencode (str) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: travc
+  // +      input by: Brett Zamir (http://brett-zamir.me)
+  // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +      input by: Michael Grier
+  // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: Ratheous
+  // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+  // +   bugfixed by: Joris
+  // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+  // %          note 1: This reflects PHP 5.3/6.0+ behavior
+  // %        note 2: Please be aware that this function expects to encode into UTF-8 encoded strings, as found on
+  // %        note 2: pages served as UTF-8
+  // *     example 1: rawurlencode('Kevin van Zonneveld!');
+  // *     returns 1: 'Kevin%20van%20Zonneveld%21'
+  // *     example 2: rawurlencode('http://kevin.vanzonneveld.net/');
+  // *     returns 2: 'http%3A%2F%2Fkevin.vanzonneveld.net%2F'
+  // *     example 3: rawurlencode('http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a');
+  // *     returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
+  str = (str + '').toString();
+
+  // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+  // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+  return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+  replace(/\)/g, '%29').replace(/\*/g, '%2A');
+}
+
+
+	function emailCurrentPage() {
+
+  var thispage= rawurlencode(window.location);
+   
+  
+ var title = "United Product Guide" 
+  //document.title;
+  
+	  window.location.href = "mailto:?subject="+title+"&body="+ thispage ;
+
+	  console.log(thispage);
+         
+};
+
+	$('#list-email').click(emailCurrentPage);
+
+
+
 	// Get Page URL and set active links
 
 	$(function(){	
@@ -436,9 +486,28 @@ url = $.url(window.location.pathname);
   window.location = 'http://54.227.239.49/';
 });
 
+	$('#list-search').click(function(){
+  window.location = '/search';
+});
+
 	$('#planes').next().addClass("firstLevel");
 	
-	$(".accordion").next().addClass("drop_down");   
+	$(".accordion").next().addClass("drop_down"); 
+	
+	   $("<div id='flying_plane'></div> ").appendTo("#home-wrap")
+
+		$("<div id='landing_plane'></div> ").appendTo("#landing-wrap")
+	 
+function printPage()
+{
+  // remove elements for printing
+   $(".config-menu").remove();
+   
+    window.print();
+    }  
+    
+    $('#list-print').click( printPage );
+
 
 ///  slide show config
 
